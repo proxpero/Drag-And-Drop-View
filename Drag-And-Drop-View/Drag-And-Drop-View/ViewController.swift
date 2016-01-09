9571c063215b81ffbafaeca3deb419ee42a66b65
Todd Olsen <todd.olsen@me.com>
2016-01-09 13:30:16 -0500

    Create project in Xcode

---
+++ Drag-And-Drop-View/Drag-And-Drop-View/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Drag-And-Drop-View
+//
+//  Created by Todd Olsen on 1/9/16.
+//  Copyright © 2016 Todd Olsen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
